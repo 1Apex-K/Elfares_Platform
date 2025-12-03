1b90a4760faebbeb9a198454933d8675923aac14
Kareem mahmoud <kareemmahmoud276@gmail.com>
2025-12-03 17:09:00 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,24 +1,16 @@
 <!-- firebase.js -->
 <script type="module">
-  // Firebase SDK imports
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
   import { 
-    getAuth, 
-    createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword 
+    getAuth 
   } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
-
   import { 
-    getFirestore, 
-    doc, 
-    setDoc 
+    getFirestore 
   } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
-
-  import {
-    getStorage
+  import { 
+    getStorage 
   } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
 
-  // Firebase config
   const firebaseConfig = {
     apiKey: "AIzaSyBboohz_RfwN48uIR5PuLSHk1hrE3mlFik",
     authDomain: "elfares-platform.firebaseapp.com",
@@ -29,13 +21,11 @@
     measurementId: "G-PFV5L7Y29W"
   };
 
-  // Init
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const db = getFirestore(app);
   const storage = getStorage(app);
 
-  // Expose globally
   window.firebaseAuth = auth;
   window.firebaseDB = db;
   window.firebaseStorage = storage;
